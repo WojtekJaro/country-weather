32e0e647f5b9cded594663141c310fb758ecb59c
Dawid <dawtom97@gmail.com>
2023-07-07 11:30:44 +0200

    countries fetching

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,11 +1,34 @@
-import React from 'react'
+import { Container, Grid, Typography } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import Country from "../components/Country";
 
 const HomePage = () => {
-  return (
-    <div>
-      <h1>Home Page</h1>
-    </div>
-  )
-}
+  const [countries, setCountries] = useState([]);
 
-export default HomePage
+  useEffect(() => {
+    getCountriesData();
+  }, []);
+
+  const getCountriesData = async () => {
+    const response = await fetch("https://restcountries.com/v3.1/all");
+    const data = await response.json();
+    setCountries(data);
+  };
+
+  console.log(countries);
+
+  return (
+    <Container maxWidth="xl">
+      <Typography variant="h3">All Countries</Typography>
+      <Grid container spacing={2}>
+        {countries.map((country) => (
+          <Grid item md={3} sm={6} xs={12}>
+            <Country item={country} />
+          </Grid>
+        ))}
+      </Grid>
+    </Container>
+  );
+};
+
+export default HomePage;
