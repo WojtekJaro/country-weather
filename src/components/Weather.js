2d33c4aa3c0b955b2ff89ffc555731d9ce226627
Wojtek <wwojciechjarocki@gmail.com>
2023-07-10 11:04:25 +0200

    temp bugfix

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,4 +1,28 @@
+import { Slider, Typography } from '@mui/material'
 import React, { useEffect, useState } from 'react'
+
+function valuetext(value) {
+	return `${value}°C`
+}
+
+const marks = [
+	{
+		value: 0,
+		label: '0°C',
+	},
+	{
+		value: 20,
+		label: '20°C',
+	},
+	{
+		value: 37,
+		label: '37°C',
+	},
+	{
+		value: 100,
+		label: '100°C',
+	},
+]
 
 const Weather = ({ capital }) => {
 	const getWeather = async () => {
@@ -9,26 +33,51 @@ const Weather = ({ capital }) => {
 		setWeather(data)
 	}
 	const [weather, setWeather] = useState([])
+	const [temp, setTemp] = useState()
 
 	useEffect(() => {
 		getWeather()
 	}, [capital])
+	useEffect(() => {
+		const celcius = (weather.name?.temp - 273.15).toFixed()
+		setTemp(celcius)
+		
+
+	}, [weather])
+
 	if (weather.length === 0) {
 		return 'Loading'
 	}
 
-	const convertTemp = temp => {
-		return (temp - 273.15).toFixed();
-	}
-
-	console.log(weather)
+	// 	const convertTemp = temp => {
+	// 		return (temp - 273.15).toFixed()
+	// 	}
+	// 	const currentTemp = convertTemp(weather.main.temp);
+	// console.log(currentTemp)
+	// 	console.log(weather)
 
 	return (
 		<div>
-			<h2>Nazwa miasta: {weather.name} </h2>
-			<h2>Temperatura: {convertTemp(weather.main.temp)}℃ </h2>
-			<h2>Wilgotność: {weather.main.humidity} % </h2>
-			<h2>Ciśnienie: {weather.main.pressure} hpa </h2>
+			<Typography gutterBottom variant='h6' component='div'>
+				{weather.name.common}
+			</Typography>
+			<Typography variant='body2' color='text.secondary'>
+				{/* <strong> Temperatura: </strong> {convertTemp(weather.main.temp)}℃ */}
+			</Typography>
+			<Typography variant='body2' color='text.secondary'>
+				<strong> Wilgotność: </strong> {weather.main.humidity} %
+			</Typography>
+			<Typography variant='body2' color='text.secondary'>
+				<strong> Ciśnienie: </strong> {weather.main.pressure} hpa
+			</Typography>
+			<Slider
+				aria-label='Custom marks'
+				defaultValue={temp}
+				getAriaValueText={valuetext}
+				step={10}
+				valueLabelDisplay='auto'
+				marks={marks}
+			/>
 		</div>
 	)
 }
--- src/pages/CountryPage.js
+++ src/pages/CountryPage.js
@@ -1,107 +1,140 @@
-import { Button, ButtonGroup, CardMedia, Container, Grid, List, ListItem, Typography } from '@mui/material'
-import React, { useEffect, useState, useTransition } from 'react'
-import { Link, useLocation, useNavigate } from 'react-router-dom'
-import Weather from '../components/Weather'
-
-const CountryPage = () => {
+import {
+	Button,
+	ButtonGroup,
+	CardMedia,
+	Container,
+	Grid,
+	List,
+	ListItem,
+	Typography,
+  } from "@mui/material";
+  import React, { useEffect, useState } from "react";
+  import { Link, useLocation, useNavigate } from "react-router-dom";
+  import Weather from "../components/Weather";
+  
+  const CountryPage = () => {
 	const {
-		state: { item },
-	} = useLocation()
-
-	const [neighbours, setNeighbours] = useState([])
-	
-
+	  state: { item },
+	} = useLocation();
+  
+	const [neighbours, setNeighbours] = useState([]);
+  
 	useEffect(() => {
-		if (!item.borders) return
-		getNeighbours()
-	}, [item.borders])
-
-	const navigate = useNavigate()
-
-	const handleNavigate = neighbour => {
-		const slug = neighbour.name.common.replaceAll(' ', '-').toLowerCase()
-		navigate(`/country/${slug}`, { state: { item: neighbour } })
-	}
-
-	console.log(neighbours)
-
-	const nativeKey = Object.keys(item.name.nativeName)[0]
-	const nativeName = item.name.nativeName[nativeKey]
-	const languages = Object.values(item.languages)
-	const currenciesKey = Object.keys(item.currencies)[0]
-	const currencies = item.currencies[currenciesKey]
-
+	  if (!item.borders) return;
+	  getNeighbours();
+	}, [item.borders]);
+  
+	const navigate = useNavigate();
+  
+	const handleNavigate = (neighbour) => {
+	  const slug = neighbour.name.common.replaceAll(" ", "-").toLowerCase();
+	  navigate(`/country/${slug}`, { state: { item: neighbour } });
+	};
+  
+	console.log(neighbours);
+  
+	const nativeKey = item.name.nativeName
+	  ? Object.keys(item.name.nativeName)[0]
+	  : null;
+	const nativeName = nativeKey
+	  ? item.name.nativeName[nativeKey]
+	  : "Nie dotyczy";
+	const languages = item.languages ? Object.values(item.languages) : [];
+	const currenciesKey = item.currencies
+	  ? Object.keys(item.currencies)[0]
+	  : null;
+	const currencies = currenciesKey ? item.currencies[currenciesKey] : "brak";
+  
+	console.log(languages)
+  
 	const getNeighbours = async () => {
-		const borders = item.borders.join(',')
-		const response = await fetch(`https://restcountries.com/v3.1/alpha?codes=${borders}`)
-		const data = await response.json()
-		setNeighbours(data)
-	}
-
+	  const borders = item.borders.join(",");
+	  const response = await fetch(
+		`https://restcountries.com/v3.1/alpha?codes=${borders}`
+	  );
+	  const data = await response.json();
+	  setNeighbours(data);
+	};
+  
 	return (
-		<Container maxWidth='xl'>
-			<Button  sx={{ margin: '30px 0' }} variant='outlined'>
-				<Link to='/'>Wróć</Link>
-			</Button>
-
+	  <Container maxWidth="xl">
+		<Button sx={{ margin: "30px 0" }} variant="outlined">
+		  <Link to="/">Wróć</Link>
+		</Button>
+  
+		<Grid container spacing={2}>
+		  <Grid item xs={12} md = {6}>
+			<img style={{width: "100%"}} src={item.flags.png} alt="" />
+			{/* <CardMedia
+			  sx={{ height: 400, width: "100%" }}
+			  image={item.flags.png}
+			  title="green iguana"
+			/> */}
+			
+		  </Grid>
+		  <Grid item xs={12} md={6}>
+			<Typography variant="h2">{item.name.common} </Typography>
 			<Grid container spacing={2}>
-				<Grid item xs={6}>
-					<CardMedia sx={{ height: 400 }} image={item.flags.png} title='green iguana' />
-				</Grid>
-				<Grid item xs={6}>
-					<Typography variant='h2'>{item.name.common} </Typography>
-					<Grid container spacing={2}>
-						<Grid item xs={6}>
-							<Typography variant='subtitle1'>
-								<strong>Lokalna nazwa: </strong>
-								{nativeName.official}{' '}
-							</Typography>
-							<Typography variant='subtitle1' component='span'>
-								<strong>Region:</strong> {item.region}{' '}
-							</Typography>
-
-							<Typography sx={{ mt: 4, mb: 2 }} variant='h6' component='div'>
-								<strong>Języki:</strong>
-							</Typography>
-
-							<List>
-								{languages.map(language => (
-									<ListItem key={language}>
-										<Typography>- {language}</Typography>
-									</ListItem>
-								))}
-							</List>
-						</Grid>
-						<Grid item xs={6}>
-							<Typography variant='subtitle1' component='span'>
-								<strong>Region: </strong>
-								{item.subregion}{' '}
-							</Typography>
-							<br />
-							<Typography variant='subtitle1' component='span'>
-								<strong>Stolica:</strong>: {item.capital}{' '}
-							</Typography>
-
-							<br />
-							<Typography variant='subtitle1' component='span'>
-								<strong>Waluta:</strong> {currencies.name}{' '}
-							</Typography>
-						</Grid>
-					</Grid>
-
-					<ButtonGroup sx={{display:'flex',flexWrap:'wrap',gap:'5px'}} size='medium' aria-label='large button group'>
-						{neighbours.map(neighbour => (
-							<Button onClick={() => handleNavigate(neighbour)} key={neighbour.name.common}>
-								{neighbour.name.common}
-							</Button>
-						))}
-					</ButtonGroup>
-				</Grid>
+			  <Grid item xs={12} md={6}>
+				<Typography variant="subtitle1">
+				  <strong>Lokalna nazwa: </strong>
+				  {nativeName.official ?? "Nie dotyczy"}{" "}
+				</Typography>
+				<Typography variant="subtitle1" component="span">
+				  <strong>Region:</strong> {item.region ?? "Nie dotyczy"}{" "}
+				</Typography>
+  
+				<Typography sx={{ mt: 4, mb: 2 }} variant="h6" component="div">
+				  <strong>Języki:</strong>
+				</Typography>
+  
+				<List>
+				  {languages.length > 0
+					? languages.map((language) => (
+						<ListItem key={language}>
+						  <Typography>- {language}</Typography>
+						</ListItem>
+					  ))
+					: "Brak"}
+				</List>
+			  </Grid>
+			  <Grid item xs={12} md={6}>
+				<Typography variant="subtitle1" component="span">
+				  <strong>Region: </strong>
+				  {item.subregion ?? "Nie dotyczy"}{" "}
+				</Typography>
+				<br />
+				<Typography variant="subtitle1" component="span">
+				  <strong>Stolica:</strong>: {item.capital ?? "Nie dotyczy"}{" "}
+				</Typography>
+  
+				<br />
+				<Typography variant="subtitle1" component="span">
+				  <strong>Waluta:</strong> {currencies.name ?? "Nie dotyczy"}{" "}
+				</Typography>
+				{item.capital && <Weather capital={item.capital} />}
+			  </Grid>
+			  
 			</Grid>
-			<Weather capital={item.capital}/>
-		</Container>
-		
-	)
-}
-
-export default CountryPage
+  
+			<ButtonGroup
+			  sx={{ display: "flex", flexWrap: "wrap", gap: "5px" }}
+			  size="medium"
+			  aria-label="large button group"
+			>
+			  {neighbours.map((neighbour) => (
+				<Button
+				  onClick={() => handleNavigate(neighbour)}
+				  key={neighbour.name.common}
+				>
+				  {neighbour.name.common}
+				</Button>
+			  ))}
+			</ButtonGroup>
+		  </Grid>
+		</Grid>
+	  </Container>
+	);
+  };
+  
+  export default CountryPage;
